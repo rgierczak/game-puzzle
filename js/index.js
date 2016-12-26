7d40ce16b432e089a97211321eafd1b60063c8ce
Rafał Gierczak <grime85@gmail.com>
2016-12-26 02:42:43 +0100

    Build puzzle wrapper

--- js/index.js
+++ js/index.js
@@ -1,6 +1,44 @@
+const CUBE_SIZE = 30;
+const CUBE_ROW_AMOUNT = 16;
+
 class Puzzle {
     constructor() {
-        console.log('puzzle');
+        this.$puzzleWrapper = null;
+        
+        this.setupPuzzle();
+    }
+    
+    setupPuzzle() {
+        this.addPuzzleWrapper();
+        this.addElementsToPuzzleWrapper();
+    }
+    
+    addPuzzleWrapper() {
+        this.$puzzleWrapper = document.createElement('div');
+        this.$puzzleWrapper.setAttribute('id', 'puzzle');
+        this.$puzzleWrapper.style.width = CUBE_SIZE * Math.sqrt(CUBE_ROW_AMOUNT) + 'px';
+        document.body.appendChild(this.$puzzleWrapper);
+    }
+    
+    addElementsToPuzzleWrapper() {
+        for (let i = 0; i < CUBE_ROW_AMOUNT - 1; i++) {
+            this.buildPuzzleElement(i);
+        }
+    }
+    
+    buildPuzzleElement(i) {
+        let $element = document.createElement('div');
+        $element.setAttribute('class', 'element');
+        $element.setAttribute('id', String(i));
+        $element.style.width = (CUBE_SIZE - 2) + 'px';
+        $element.style.height = (CUBE_SIZE - 2) + 'px';
+    
+        $element.addEventListener('click', this.clickElementHandler.bind(this));
+        this.$puzzleWrapper.appendChild($element);
+    }
+    
+    clickElementHandler(event) {
+        console.log('element: ', event.target.id);
     }
 }
 
