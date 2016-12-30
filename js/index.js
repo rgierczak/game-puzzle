baaa3a831ea380003241d01ffe741c4d75d058c6
Rafał Gierczak <grime85@gmail.com>
2016-12-30 23:56:06 +0100

    Set absolute position of puzzle element

--- js/index.js
+++ js/index.js
@@ -31,13 +31,24 @@ class Puzzle {
     
     shuffleElements() {
         let shuffledElements = Utils.shuffle(this.puzzles);
-        console.log(shuffledElements);
     }
     
     displayElements() {
-        this.puzzles.forEach((element) => {
-            this.render(this.$puzzleWrapper, element.puzzleElement.$node);
+        this.puzzles.forEach((element, index) => {
+            this.displayElement(element, index);
         })
+    }
+    
+    displayElement(element, index) {
+        let $element = this.setElementPosition(element, index);
+        this.render(this.$puzzleWrapper, $element);
+    }
+    
+    setElementPosition(element, index) {
+        let $el = element.puzzleElement.$node;
+        $el.style.left = CUBE_SIZE * (index % 4) + 'px';
+        $el.style.top = CUBE_SIZE * Math.floor(index / 4) + 'px';
+        return $el;
     }
     
     render($wrapper, $element) {
