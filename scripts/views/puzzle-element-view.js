17157ca7eee322d25dea736f087fd8b14fd2013a
Rafał Gierczak <grime85@gmail.com>
2017-02-06 17:14:03 +0100

    Remove commented code

--- scripts/views/puzzle-element-view.js
+++ scripts/views/puzzle-element-view.js
@@ -3,14 +3,6 @@
     
     class PuzzleElementView {
         constructor(model) {
-            // this.model = {
-            //     $node: null,
-            //     index: null
-            // };
-            
-            // this.createElement(index);
-            // this.addListener();
-            
             this.template = null;
             this.render(model);
         }
@@ -23,34 +15,7 @@
             this.template = document.createElement('div');
             
             console.log('template: ', this.template);
-            // this.setElementAttributes(index);
-            // this.setElementDimensions();
-            // this.setElementText(index);
         }
-        
-        // addListener() {
-        //     this.template.addEventListener('click', this.clickElementHandler.bind(this));
-        // }
-        //
-        // clickElementHandler(event) {
-        //     console.log('element: ', event.target.id);
-        // }
-        //
-        // setElementText(index) {
-        //     this.template.innerText = index;
-        // }
-        //
-        // setElementAttributes(index) {
-        //     let $node = this.template;
-        //     $node.setAttribute('class', 'element');
-        //     $node.setAttribute('id', index.toString());
-        // }
-        //
-        // setElementDimensions() {
-        //     let $node = this.template;
-        //     $node.style.width = (CUBE_SIZE - 2) + 'px';
-        //     $node.style.height = (CUBE_SIZE - 2) + 'px';
-        // }
     }
     
     root.puzzle.views.PuzzleElementView = PuzzleElementView;
